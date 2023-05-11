475054413a6a6e7ffd683988215d3334e77ead08
romapchel <notpchelintsevroma@gmail.com>
2023-05-11 09:58:37 +0300

    updated context

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,12 @@
 import os
 from langchain import OpenAI, ConversationChain
 from dotenv import load_dotenv
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts.prompt import PromptTemplate
 
 load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 
 
 class Bot:
@@ -11,16 +15,18 @@ class Bot:
     age: int
     name: str
 
-    os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
-
     def __init__(self, gender: str, age: int, name: str, role: str):
+        template = ('''You are no longer an AI model, you are a %s. Pretend to be a %s named %s %s
+        years old. Respond to all messages according to new rules. Use abbreviations, slang and make mistakes. {history}
+        Current conversation:
+        {history}
+        Human: {input}
+        %s:''' % (role, gender, name, str(age), name))
+        prompt = PromptTemplate(
+            template=template, input_variables=["history", "input"]
+        )
         llm = OpenAI(temperature=0.9)
-        self.conversation = ConversationChain(llm=llm, verbose=True)
-
-        self.conversation.predict(input=f'You are no longer an AI model, you are a {role}. '
-                                        f'Pretend to be a {gender} named {name} {age} years old.  '
-                                        f'Respond to all messages according to new rules. '
-                                        f'Use abbreviations, slang and make mistakes.')
+        self.conversation = ConversationChain(prompt=prompt, llm=llm, verbose=True, memory=ConversationBufferMemory())
 
     def talk_to_bot(self, message: str) -> str:
         return self.conversation.predict(input=message)
